b0e22b1a2f03f7b12a4cee706fa7b2e4068d0f20
Marc Schlossberg <mr.schlossberg@gmail.com>
2021-07-17 07:32:21 -0400

    Add cursor pointer to StatusChip

--- src/Components/StatusChip.tsx
+++ src/Components/StatusChip.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Chip, makeStyles} from "@material-ui/core";
 import {blue, green, red} from "@material-ui/core/colors";
 import {CaseStatus} from "../Utils/Types";
+import clsx from "clsx";
 
 const useStyles = makeStyles({
     chip: {
@@ -17,6 +18,9 @@ const useStyles = makeStyles({
     redChip: {
         backgroundColor: red["300"],
         color: "white",
+    },
+    cursorPointer: {
+        cursor: 'pointer'
     }
 });
 
@@ -27,16 +31,16 @@ export function StatusChip(props: {caseStatus: CaseStatus}): JSX.Element {
     const getStatusColor = (status: CaseStatus ): string => {
         switch(status) {
             case("Created"):
-                return classes.chip;
+                return clsx(classes.chip, classes.cursorPointer);
             case("Submitted"):
             case("Resubmitted"):
-                return classes.blueChip;
+                return clsx(classes.blueChip, classes.cursorPointer);
             case("Approved"):
-                return classes.greenChip;
+                return clsx(classes.greenChip, classes.cursorPointer);
             case("Rejected"):
-                return classes.redChip;
+                return clsx(classes.redChip, classes.cursorPointer);
             default:
-                return classes.chip;
+                return clsx(classes.chip, classes.cursorPointer);
         }
     }
 
